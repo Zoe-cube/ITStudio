4f843205ed033dbc53eb6fcc8b18a516a4e92893
Zoe-cube <54666031+Zoe-cube@users.noreply.github.com>
2019-11-23 20:23:35 +0800

    Add files via upload

--- src/Body1/Container3.js
+++ src/Body1/Container3.js
@@ -13,13 +13,13 @@ class Container3 extends Component {
                             <p id='word3-1-3'>爱特一直以聚集和培养培养计算机人才为己任，发掘技术潜力，感受合作乐趣，让项目高效执行，让大家一同进步。</p>
                         </div>
                         <div id='li3-1'>
-                            <div id='pic3-1'></div>
+                            <div id='pic3-2'></div>
                             <p id='word3-1-1'>优秀创意<br></br><span>灵感建构未来</span></p>
                             
                             <p id='word3-1-3'>用创意打磨精巧设计，用智慧实现炫酷交互；用热情呈现高效代码，以交流触碰灵感火花；以创造激发全新想象，用双手创造改变未来。</p>
                         </div>
                         <div id='li3-1'>
-                            <div id='pic3-1'></div>
+                            <div id='pic3-3'></div>
                             <p id='word3-1-1'>乐在其中<br></br><span>洞察技术玄机</span></p>
                             
                             <p id='word3-1-3'>汇聚技术力量，解剖程序内核，研究代码本质，巧解应用难题。以逻辑洞察数据，以细节战胜漏洞。</p>
@@ -33,11 +33,11 @@ class Container3 extends Component {
 多样的部门满足丰富的兴趣。在这里，不论年龄，不论性别，只要你有一颗学技术的心，爱特工作室总能给予你惊喜！
                          </div>
                         <div id='ul3-2'>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
+                            <div id='li3-2-1'></div>
+                            <div id='li3-2-2'></div>
+                            <div id='li3-2-3'></div>
+                            <div id='li3-2-4'></div>
+                            <div id='li3-2-5'></div>
                         </div>
                     </div>
                     {/* 成员介绍 */}
@@ -49,11 +49,11 @@ class Container3 extends Component {
 加入我们，和这些有趣又万能的灵魂在一起，把思考与责任铭记于心，把对技艺的热爱传承下去。
                          </div>
                         <div id='ul3-2'>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
-                            <div id='li3-2'></div>
+                            <div id='li3-2-6'><header>14'</header><p>2014届</p></div>
+                            <div id='li3-2-7'><header>15'</header><p>2015届</p></div>
+                            <div id='li3-2-8'><header>16'</header><p>2016届</p></div>
+                            <div id='li3-2-9'><header>17'</header><p>2017届</p></div>
+                            <div id='li3-2-10'><header>18'</header><p>2018届</p></div>
                         </div>
                     </div>
                     {/* 历史介绍 */}
@@ -65,10 +65,10 @@ class Container3 extends Component {
 加入我们，和这些有趣又万能的灵魂在一起，把思考与责任铭记于心，把对技艺的热爱传承下去。
                          </div>
                         <div id='ul3-3'>
-                            <div id='li3-4'></div>
-                            <div id='li3-3'></div>
-                            <div id='li3-3'></div>
-                            <div id='li3-3'></div>
+                            <div id='li3-4'><p id='p'>部门拆分</p><header id='h'>UI设计<br></br>前端开发</header></div>
+                            <div id='li3-3'><p id='p'>新增成员</p><header id='h'>6人</header></div>
+                            <div id='li3-3'><p id='p'>项目上线</p><header id='h'>中国海洋大学<br></br>iGEM比赛团队<br></br>展示页面</header></div>
+                            <div id='li3-3'><p id='p'>项目上线</p><header id='h'>海洋技术系网站<br></br>上线运营</header></div>
                         </div>
                         <div id='ul3-5'>
                             <div id='li3-5'>2014</div>
@@ -83,7 +83,12 @@ class Container3 extends Component {
                         <header>作品介绍 Works</header>
                         <p>网页作品系列</p>
                         <div id='container3-3-1'>
-                            hkj
+                           <div></div>
+                           <div></div>
+                           <div></div>
+                           <div></div>
+                           <div></div>
+                           <div></div>
                         </div>
                     </div>
                     {/* 留言板 */}
@@ -92,11 +97,14 @@ class Container3 extends Component {
                         <p>在这里留下你的宝贵意见</p>
                         <div >
                             <div id='roll3-4'>
-                                <div id='con3-4'></div>
-                                <div id='con3-4'></div>
-                                <div id='con3-4'></div>
+                                <div id='con3-4'>人生好难</div>
+                                <div id='con3-4'>来，抱一下，不准哭了</div>
+                                <div id='con3-4'>楼上真秀</div>
                             </div>
                             <div id='blank3-4'>
+                                <div id='saysth'>
+                                    写点什么...
+                                </div>
                                 
                             </div>
                         </div>
